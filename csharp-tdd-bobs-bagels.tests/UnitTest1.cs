55935f0b0d6003474d319f7b034d6d68816f48ba
Nigel <nigel.sibbert@gmail.com>
2024-01-15 10:15:00 +0000

    updated test project/

---
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace csharp_tdd_bobs_bagels.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
